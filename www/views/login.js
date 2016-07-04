57969fe99294494904592a123d897f648637ba99
cellhone <okita@macbook.local>
2016-07-04 09:48:18 +0900

    login

--- www/views/login.js
+++ www/views/login.js
@@ -1,5 +1,44 @@
 angular.module('starter')
 
 .controller('LoginCtrl', function($scope) {
-  console.log('LoginCtrl');
+  $scope.imgsrc="http://xmpp.hplabs.jp/demo/push/lib/avator/makoto.jpg";
+  
+  $scope.clickExpand = function() {
+    console.log('clickExpand');
+//    if(!isFullscreen) {
+//        var body = document.body;
+//        if (body.webkitRequestFullScreen) {
+//            body.webkitRequestFullScreen();
+//        } else if (body.mozRequestFullScreen) {
+//            body.mozRequestFullScreen();
+//        } else {
+//            body.requestFullScreen();
+//        }
+//        isFullscreen = true;
+//    }
+//    else {
+//        /// 解除
+//        if (document.webkitCancelFullScreen) {
+//            document.webkitCancelFullScreen();
+//        } else if (document.mozCancelFullScreen) {
+//            document.mozCancelFullScreen();
+//        } else {
+//            document.exitFullscreen();
+//        }
+//        isFullscreen = false;
+//    }
+//    location.reload();
+  }
+  
+  $scope.clicImage = function() {
+    console.log('clicImage');
+  }
+  
+  $scope.clickLogin = function() {
+    console.log('clickLogin');
+  }
+  
+  
+  var isFullscreen = false;
+
 });
